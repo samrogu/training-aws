488a45e14ac17f8f4893fc1f1622a990b20d1db3
Samuel Guardado <grd.rodrigez13@gmail.com>
2024-09-28 12:14:39 -0600

    Backend S3

--- vpc/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "saguro-training-aws"
-    key = "rapid/vpc/terraform.tfstate" 
-  }
-}
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-${var.product}-${terraform.workspace}"
+  name = "vpc-${var.product}"
   cidr = var.vpc_cdir
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
@@ -14,6 +14,6 @@ module "vpc" {
 
   tags = {
     Terraform = "true"
-    Environment = "${var.product}-${terraform.workspace}"
+    Environment = "${var.product}"
   }
 }
